922096d4eb22b8f737ac02be6b2d722e4f42aa28
Tom Ward <tomward@fmail.co.uk>
2017-07-21 14:48:20 +0100

    [wip] add some more bits

--- app/components/pubs/pubs_router.js
+++ app/components/pubs/pubs_router.js
@@ -2,6 +2,7 @@ export default Mn.AppRouter.extend({
   appRoutes: {
     'publications/:id': 'showPub',
     'publications': 'listPubs',
+    'publicationsd3': 'd3Pubs',
     'publications/new/': 'newPub',
     'publications/:id/edit/details': 'editPubDetails',
     'publications/:id/edit/content': 'editPubContent'

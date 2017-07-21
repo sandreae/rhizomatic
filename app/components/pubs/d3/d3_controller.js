62ba9455e6205d95c2dba60c5b9bb5f421b19ae2
Tom Ward <tomward@fmail.co.uk>
2017-07-21 14:33:15 +0100

    [wip] try to get d3 up & running

---
+++ app/components/pubs/d3/d3_controller.js
@@ -0,0 +1,17 @@
+import {TableView} from './views/list_view'
+import {gc} from '../../radio'
+
+var Controller = {
+  d3Pubs: function() {
+
+   $.when(gc.request('pubs:get')).done(function (pubs) {
+      var tableView = new TableView({
+        collection: pubs
+      })
+
+      Platform.Regions.getRegion('main').show(tableView)
+    })
+  }
+}
+
+export {Controller}

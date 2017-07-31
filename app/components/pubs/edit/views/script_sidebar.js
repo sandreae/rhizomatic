3f4f6d4f6ffea0c424888a56b8721fd339c20b2b
Sam Andreae <sandreae@gmail.com>
2017-07-31 02:25:50 +0200

    url upload added

--- app/components/pubs/edit/views/script_sidebar.js
+++ app/components/pubs/edit/views/script_sidebar.js
@@ -6,17 +6,23 @@ var ScriptSidebar = Marionette.View.extend({
 
   events: {
     'click button.js-submit': 'submitClicked',
-    'click button.js-publish': 'publishClicked'
+    'click button.js-publish': 'publishClicked',
+    'click button.js-update': 'updateUrl'
   },
 
   behaviors: {
     validate: Platform.Behaviours.FormValidate,
   },
 
+  initialize: function() {
+  	var url = this.model.get('activeContent')
+  	this.showIframe(url)
+  },
+
   submitClicked: function(e) {
     e.preventDefault()
-    var content = 'url here'
     var data = Backbone.Syphon.serialize(this);
+    var content = data.url
     this.trigger('form:submit', content, data, this.model)
 
     //nvar data = Backbone.Syphon.serialize(this);
@@ -27,6 +33,23 @@ var ScriptSidebar = Marionette.View.extend({
     this.model.set({published: true})
     this.submitClicked(e)
   },
+
+  updateUrl: function(e) {
+  	e.preventDefault()
+  	var data = Backbone.Syphon.serialize(this);
+  	var url = data.url
+  	this.showIframe(url)
+	},
+
+  showIframe:  function(url) {
+  	$('#placeholder').empty();
+	var container = document.getElementById("placeholder");
+    var ifrm = document.createElement("iframe");
+    ifrm.setAttribute("src", url)
+    ifrm.style.width = "640px";
+    ifrm.style.height = "480px";
+    container.append(ifrm)
+  }
 })
 
 export {ScriptSidebar}

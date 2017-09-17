1eaa01a1c8790ccf25b1687345e41aae6b08987a
Sam Andreae <sandreae@gmail.com>
2017-09-17 10:03:41 +0200

    analytics

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import * as Behaviors from './entities/behaviors/behaviors'
 import Layout from './theme/layout/layout'
 import {gc} from './components/radio'
+var KeenTracking = require('keen-tracking');
 
 var App = Marionette.Application.extend({
   el: 'body',
@@ -8,12 +9,23 @@ var App = Marionette.Application.extend({
 
   initialize: function() {
     this.Regions = new Layout()
+
+    // Configure a client instance
+    var client = new KeenTracking({
+      projectId: '59be28fec9e77c0001095e78',
+      writeKey: 'CDE12502085A8F310E5B23E48195B66A2701E68ED83A68B8B9A9DAF98AD70BD54B91EF6A92CFC9EBE976C55B58F5736BE7EBC661D695C27FF88848DE9B2D13C0A7AB15E595E1A2DCBCAD5D534D3AC72A7E60CBB2353C0278F38F3F8D0486FDD8'
+    });
+
+    // Record an event
+    client.recordEvent('pageviews', {
+      title: document.title
+    });
   },
 
   onStart: function() {
     var self = this
     var initPromise = gc.request('user:init')
-    initPromise.then(function(){
+    initPromise.then(function() {
       if (Backbone.history) {
         Backbone.history.start()
         if (self.getCurrentRoute() === '') {

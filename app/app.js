42de2591b4302daa0a2ccdc52fafb9c70c1e68c3
Sam Andreae <sandreae@gmail.com>
2017-09-17 11:27:56 +0200

    analytics

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import * as Behaviors from './entities/behaviors/behaviors'
 import Layout from './theme/layout/layout'
 import {gc} from './components/radio'
+import Keen from 'keen-tracking';
 
 var App = Marionette.Application.extend({
   el: 'body',
@@ -8,12 +9,78 @@ var App = Marionette.Application.extend({
 
   initialize: function() {
     this.Regions = new Layout()
+
+    const client = new Keen({
+      projectId: '59be28fec9e77c0001095e78',
+      writeKey: 'CDE12502085A8F310E5B23E48195B66A2701E68ED83A68B8B9A9DAF98AD70BD54B91EF6A92CFC9EBE976C55B58F5736BE7EBC661D695C27FF88848DE9B2D13C0A7AB15E595E1A2DCBCAD5D534D3AC72A7E60CBB2353C0278F38F3F8D0486FDD8'
+    });
+    const helpers = Keen.helpers;
+    const utils = Keen.utils;
+    
+    client.extendEvents(() => {
+      return {
+        geo: {
+          info: { /* Enriched */ },
+          ip_address: '${keen.ip}',
+        },
+        page: {
+          info: { /* Enriched */ },
+          title: document.title,
+          url: document.location.href
+        },
+        referrer: {
+          info: { /* Enriched */ },
+          url: document.referrer
+        },
+        tech: {
+          browser: helpers.getBrowserProfile(),
+          info: { /* Enriched */ },
+          user_agent: '${keen.user_agent}'
+        },
+        time: helpers.getDatetimeIndex(),
+        keen: {
+          addons: [
+            {
+              name: 'keen:ip_to_geo',
+              input: {
+                ip: 'geo.ip_address'
+              },
+              output : 'geo.info'
+            },
+            {
+              name: 'keen:ua_parser',
+              input: {
+                ua_string: 'tech.user_agent'
+              },
+              output: 'tech.info'
+            },
+            {
+              name: 'keen:url_parser',
+              input: {
+                url: 'page.url'
+              },
+              output: 'page.info'
+            },
+            {
+              name: 'keen:referrer_parser',
+              input: {
+                referrer_url: 'referrer.url',
+                page_url: 'page.url'
+              },
+              output: 'referrer.info'
+            }
+          ]
+        }
+      }
+    });
+     
+    client.recordEvent('pageview', {});
   },
 
   onStart: function() {
     var self = this
     var initPromise = gc.request('user:init')
-    initPromise.then(function() {
+    initPromise.then(function(){
       if (Backbone.history) {
         Backbone.history.start()
         if (self.getCurrentRoute() === '') {

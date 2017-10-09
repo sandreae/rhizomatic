fa205aa24a8e30506801821e4b25793654a04829
Sam Andreae <sandreae@gmail.com>
2017-10-09 16:04:12 +0200

    fastmail fix

--- server/router/email.js
+++ server/router/email.js
@@ -3,14 +3,25 @@ const nodemailer = require('nodemailer');
 module.exports = function (app, express) {
 
   var emailRoute = express.Router()
-
+  console.log(process.env.EMAIL_ADDRESS)
+  console.log(process.env.EMAIL_PASSWORD)
   emailRoute.get('/send',function(req,res){
   let transporter = nodemailer.createTransport({
-    service: 'FastMail',
+    host: 'smtp.fastmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_ADDRESS,
       pass: process.env.EMAIL_PASSWORD
     }
+  });
+
+  transporter.verify(function(error, success) {
+   if (error) {
+        console.log(error);
+   } else {
+        console.log('Server is ready to take our messages');
+   }
   });
 
   // setup email data with unicode symbols
